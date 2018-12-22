48daf8654ee9bdc2b284f23cc239375a85a7b2c0
Anastasia <anastasia.perminova@mifort.org>
2018-12-22 21:17:54 +0300

    fix bugs

--- models/Users.js
+++ models/Users.js
@@ -4,25 +4,19 @@ module.exports = (sequelize, DataTypes) => {
 
   const Users = sequelize.define('users', {
 
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true
-    },
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
 
-    username: {
-      type: DataTypes.STRING(100),
-      allowNull: false
+      username: {
+        type: DataTypes.STRING(100),
+        allowNull: false
+      },
     },
-  }
-  {
-    tableName: 'users',
-      timestamps
-  :
-    true,
-
-  }
-)
-  ;
+    {
+      tableName: 'users', timestamps: true,
+    });
   return Users
 };
