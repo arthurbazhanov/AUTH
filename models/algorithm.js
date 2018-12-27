ec47e0a811b80d066ec18ae31b8097b5d1d6d963
Anastasia <anastasia.perminova@mifort.org>
2018-12-27 19:31:46 +0300

    create algorithm

---
+++ models/algorithm.js
@@ -0,0 +1,26 @@
+// let max = 16;
+// let arr = [];
+// // max++;
+//
+// while (max--) {
+//   let i = arr.length;
+//   let num = Math.floor(Math.random() * (++i));
+//   arr.splice(num, 0, max);
+// }
+// console.log(arr.join(",").match(/\d+,\d+/g).join("\n"));
+
+const teams = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+const initialTeamsLength = teams.length;
+let result = new Array(Math.floor(teams.length / 2));
+for (let i = 0; i < initialTeamsLength; i++) {
+  let randomTeam = teams.splice(Math.floor(Math.random() * teams.length), 1)[0];
+  console.log(randomTeam);
+  if (i % 2) {
+    result[Math.floor(i/2)][1] = randomTeam;
+  } else {
+    result[Math.floor(i/2)] = [randomTeam];
+  }
+}
+
+console.log('result');
+console.log(...result);
