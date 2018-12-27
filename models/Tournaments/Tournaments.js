718d146b3e60014f045e2330353e69bd3113f98c
Anastasia <anastasia.perminova@mifort.org>
2018-12-27 12:29:27 +0300

    create table Tournaments

---
+++ models/Tournaments/Tournaments.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+
+  const Tournaments = sequelize.define('Tournaments', {
+
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+
+      nameTournament: {
+        type: DataTypes.STRING(100),
+        allowNull: false
+      },
+
+      createdAt: {
+        type: DataTypes.DATE,
+      },
+
+      updatedAt: {
+        type: DataTypes.DATE,
+      }
+    },
+    {
+      tableName: 'tournaments', timestamps: true,
+    });
+  return Tournaments;
+};
